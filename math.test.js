19c98498b8ceca99f107c3f1e8f69b1fade7660e
Gursimar Singh Chawla <45138974+UchihaSimar@users.noreply.github.com>
2025-06-23 12:34:27 +0530

    Failing Sum function purposely

--- math.test.js
+++ math.test.js
@@ -4,3 +4,7 @@ test("Testing Math Function: Sum",() => {
     expect(sum(1,2)).toBe(3);
     expect(sum(1,2)).not.toBe(4);
 })
+
+test("Failing Math Function: Sum", () => {
+    expect(sum(null,undefined)).toBe(3);
+})
